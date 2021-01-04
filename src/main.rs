32226c80dc010c5236fd9d7b00eed04f3f7223cd
Clayton Hickey <clayton@claytondoesthings.xyz>
2021-01-04 10:59:05 -0500

    Added persistent username/email on auth failure for signin/up

--- src/main.rs
+++ src/main.rs
@@ -75,21 +75,33 @@ fn favicon() -> Result<NamedFile, status::NotFound<String>> {
 #[get("/sitemap.xml")]
 fn sitemap() -> Template { Template::render("sitemap", &default_context()) }
 
-#[get("/login?<error>")]
-fn login(error: Option<String>, db_conn: DbConn, cookies: Cookies) -> Template {
+#[get("/login?<error>&<username>")]
+fn login(error: Option<String>, username: Option<String>, db_conn: DbConn, cookies: Cookies) -> Template {
     let mut context = signed_in_context(&*db_conn, cookies);
     match error {
         Some(error) => context.insert("error", &clean_text(&error)),
         None => {}
     }
+    match username {
+        Some(username) => context.insert("username", &clean_text(&username)),
+        None => {}
+    }
     Template::render("login", &context)
 }
 
-#[get("/signup?<error>")]
-fn signup(error: Option<String>, db_conn: DbConn, cookies: Cookies) -> Template {
+#[get("/signup?<error>&<username>&<email>")]
+fn signup(error: Option<String>, username: Option<String>, email: Option<String>, db_conn: DbConn, cookies: Cookies) -> Template {
     let mut context = signed_in_context(&*db_conn, cookies);
     match error {
         Some(error) => context.insert("error", &clean_text(&error)),
+        None => {}
+    }
+    match username {
+        Some(username) => context.insert("username", &clean_text(&username)),
+        None => {}
+    }
+    match email {
+        Some(email) => context.insert("email", &clean_text(&email)),
         None => {}
     }
     Template::render("signup", &context)
@@ -122,15 +134,16 @@ fn update_session_logged_in_user(user: models::User, mut cookies: Cookies, db_co
 
 #[post("/login", data = "<login_user>")]
 fn submit_login(db_conn: DbConn, login_user: Form<models::LoginUser>, cookies: Cookies) -> Redirect {
-    let user_query = schema::users::table.filter(schema::users::username.eq(&login_user.username)).filter(schema::users::password_hash.crypt_eq(&login_user.password));
+    let username = &login_user.username;
+    let user_query = schema::users::table.filter(schema::users::username.eq(&username)).filter(schema::users::password_hash.crypt_eq(&login_user.password));
     match user_query.first::<models::User>(&*db_conn) {
         Ok(user) => {
             match update_session_logged_in_user(user, cookies, db_conn) {
-                Ok(_) => Redirect::to(uri!(user_profile: &login_user.username)),
-                Err(_) => Redirect::to(uri!(login: "Failed to set session_id cookie.".to_string()))
+                Ok(_) => Redirect::to(uri!(user_profile: username)),
+                Err(_) => Redirect::to(uri!(login: "Failed to set session_id cookie.".to_string(), username))
             }
         },
-        Err(_) => Redirect::to(uri!(login: "Couldn't authenticate user".to_string()))
+        Err(_) => Redirect::to(uri!(login: "Couldn't authenticate user".to_string(), username))
     }
 }
 
@@ -145,42 +158,42 @@ fn submit_signup(db_conn: DbConn, new_user_form: Form<models::NewUser>, cookies:
     let username = new_user.username.clone();
     match valid_username(&username) {
         true => {
-            match validator::validate_email(email.clone()) {
+            match validator::validate_email(&email) {
                 true => {
                     match new_user.password.len() >= 1 && new_user.password.len() <= 72 {
                         true => {
                             match new_user.execute(&*db_conn) {
                                 Ok(_v) => {
                                     // TODO: Make it so custom models::NewUser insertion script is able to return models::User itself
-                                    match schema::users::table.filter(schema::users::email.eq(email)).first::<models::User>(&*db_conn) {
+                                    match schema::users::table.filter(schema::users::email.eq(&email)).first::<models::User>(&*db_conn) {
                                         Ok(user) => {
                                             match update_session_logged_in_user(user, cookies, db_conn) {
                                                 Ok(_) => Redirect::to(uri!(user_profile: &username)),
-                                                Err(_) => Redirect::to(uri!(signup: "Failed to set session_id cookie.".to_string()))
+                                                Err(_) => Redirect::to(uri!(signup: "Failed to set session_id cookie.".to_string(), username, email))
                                             }
                                         },
-                                        Err(_) => Redirect::to(uri!(signup: "Failed to retrieve new user to add them to session".to_string()))
+                                        Err(_) => Redirect::to(uri!(signup: "Failed to retrieve new user to add them to session".to_string(), username, email))
                                     }
                                 },
                                 Err(e) => {
                                     match &*e.to_string() {
-                                        "duplicate key value violates unique constraint \"users_username_key\"" => Redirect::to(uri!(signup: "Duplicate username".to_string())),
-                                        "duplicate key value violates unique constraint \"users_email_key\"" => Redirect::to(uri!(signup: "Duplicate email".to_string())),
+                                        "duplicate key value violates unique constraint \"users_username_key\"" => Redirect::to(uri!(signup: "Duplicate username".to_string(), username, email)),
+                                        "duplicate key value violates unique constraint \"users_email_key\"" => Redirect::to(uri!(signup: "Duplicate email".to_string(), username, email)),
                                         _ => {
                                             println!("Failed to add user to database: {}", e);
-                                            Redirect::to(uri!(signup: "Failed to add user to database".to_string()))
+                                            Redirect::to(uri!(signup: "Failed to add user to database".to_string(), username, email))
                                         }
                                     }
                                 }
                             }
                         },
-                        false => Redirect::to(uri!(signup: "Invalid password".to_string()))
+                        false => Redirect::to(uri!(signup: "Invalid password".to_string(), username, email))
                     }
                 }
-                false => Redirect::to(uri!(signup: "Invalid email".to_string()))
+                false => Redirect::to(uri!(signup: "Invalid email".to_string(), username, email))
             }
         },
-        false => Redirect::to(uri!(signup: "Invalid username".to_string()))
+        false => Redirect::to(uri!(signup: "Invalid username".to_string(), username, email))
     }
 }
 
@@ -221,8 +234,14 @@ fn user_profile(username: String, db_conn: DbConn, cookies: Cookies) -> Result<T
 
 #[get("/apps")]
 fn apps(db_conn: DbConn, cookies: Cookies) -> Template {
-    let mut context = signed_in_context(&*db_conn, cookies);
+    let context = signed_in_context(&*db_conn, cookies);
     Template::render("apps", &context)
+}
+
+#[get("/apps/create")]
+fn create_app(db_conn: DbConn, cookies: Cookies) -> Template {
+    let context = signed_in_context(&*db_conn, cookies);
+    Template::render("create_app", &context)
 }
 
 fn main() {
@@ -238,6 +257,7 @@ fn main() {
             signout,
             user_profile,
             apps,
+            create_app,
         ])
         .attach(DbConn::fairing())
         .attach(Template::fairing())
